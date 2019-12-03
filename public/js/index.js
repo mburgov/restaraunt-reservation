1ccc70abb07adac11a3e865d7016cdbaf5b71599
sbloody <marioburgov@gmail.com>
2019-12-03 16:53:27 +0200

    Refactoring

--- public/js/index.js
+++ public/js/index.js
@@ -5,49 +5,59 @@ const $availabilityButton = document.querySelector('#check-availability')
 
 let vacantTables;
 
-$availabilityButton.addEventListener('click', () => {
-    fetch('/reservations').then((response) => {
-        response.json().then((data) => {
-            vacantTables = data.vacantTables
-            const freedTables = data.freedTables
+$availabilityButton.addEventListener('click', async() => {
+    let result
+    try {
 
-            $freeTables.innerHTML = `Available tables: ${vacantTables}`
-            if (vacantTables > 0) {
-                $reservationButton.disabled = false
-                $nameForm.disabled = false
+        let response = await fetch('/reservations')
+        result = await response.json()
+    } catch (error) {
+        throw new Error(error)
+    }
 
-                $availabilityButton.disabled = true
+    vacantTables = result.vacantTables
+    const freedTables = result.freedTables
 
-            } else {
-                alert('All tables are taken, try again next time.')
-            }
-            if (freedTables > 0) {
-                alert(`${freedTables} tables are no longer reserved`)
-            }
-        })
-    }).catch((err) => {
-        throw new Error(err)
-    })
+    $freeTables.innerHTML = `Available tables: ${vacantTables}`
+    if (vacantTables > 0) {
+        $reservationButton.disabled = false
+        $nameForm.disabled = false
+
+        $availabilityButton.disabled = true
+
+    } else {
+        alert('All tables are taken, try again next time.')
+    }
+    if (freedTables > 0) {
+        alert(`${freedTables} tables are no longer reserved`)
+    }
 })
 
-$reservationButton.addEventListener('click', () => {
+$reservationButton.addEventListener('click', async() => {
+    let result
     const user = $nameForm.value
     $nameForm.value = ''
     if (!user) {
         return
     }
-    fetch('/reservations/' + user).then((response) => {
-        response.json().then((data) => {
-            vacantTables = data.vacantTables
-            alert('Reservation for ' + data.user + ' created')
-            $freeTables.innerHTML = `Available tables: ${vacantTables}`
-            if (vacantTables === 0) {
-                $reservationButton.disabled = true
-                $nameForm.disabled = true
-                $availabilityButton.disabled = false
-            }
+    try {
+        let response = await fetch('/reservations/' + user, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain;charset=utf-8'
+            },
+            body: user
         })
-    }).catch((error) => {
+        result = await response.json()
+    } catch (error) {
         throw new Error(error)
-    })
+    }
+    vacantTables = result.vacantTables
+    alert('Reservation for ' + result.user + ' created')
+    $freeTables.innerHTML = `Available tables: ${vacantTables}`
+    if (vacantTables === 0) {
+        $reservationButton.disabled = true
+        $nameForm.disabled = true
+        $availabilityButton.disabled = false
+    }
 })
